fbde5a8d9b866e3fad5d241ba2d2be9f87be7dce
r-barlow <rbarlow@neslotech.co.za>
2023-05-05 15:21:08 +0200

    TRAIN-265: Add app routing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+import AppLayout from "./page/AppLayout";
+
 import "./stylesheet/app.scss";
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<AppLayout/>}/>
+    ));
 
 const App = () => {
     return (
-        <>
-        </>
+        <RouterProvider router={router}/>
     );
 }
 
