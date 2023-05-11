b0a472ddfed6b6f9808f479ae26ca9f58c60c8d2
r-barlow <rbarlow@neslotech.co.za>
2023-05-11 11:44:28 +0200

    TRAIN-266: Refactor after review.

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -10,26 +10,30 @@ const Login = ({handleSubmit, onChange, form}) => {
         <div className="login__background"/>
         <section className="login__card__frame">
             <article className="login__card">
-                <div className="login__card__body">
-                    <div className="login__image__frame">
-                        <img className="login__image" src="/images/insight.svg" alt="Insight logo"/>
-                    </div>
-                    <h1 className="login__header__text">Insight</h1>
-                    <form className="login__form" onSubmit={handleSubmit}>
-                        <Input type="text" placeholder="Username"
-                               hasError={form.username.error}
-                               onChange={value => onChange({username: {value: value, error: false}})}/>
-
-                        <Input type="password" placeholder="Password"
-                               hasError={form.password.error}
-                               onChange={value => onChange({password: {value: value, error: false}})}/>
-
-                        {form.error.length !== 0 ? <p className="login__form__error">{form.error}</p> : null}
-
-                        <button type="submit" className="login__button">LOGIN
-                        </button>
-                    </form>
+                <div className="login__image__frame">
+                    <img className="login__image" src="/images/insight.svg" alt="Insight logo"/>
                 </div>
+                <h1 className="login__header__text">Insight</h1>
+                <form className="login__form" onSubmit={handleSubmit}>
+                    <Input type="text" placeholder="Username"
+                           hasError={form.username.error}
+                           onChange={value => onChange({
+                               username: {value: value, error: false},
+                               password: {...form.password, error: false}
+                           })}/>
+
+                    <Input type="password" placeholder="Password"
+                           hasError={form.password.error}
+                           onChange={value => onChange({
+                               password: {value: value, error: false},
+                               username: {...form.username, error: false}
+                           })}/>
+
+                    {form.error.length !== 0 ? <p className="login__form__error">{form.error}</p> : null}
+
+                    <button type="submit" className="login__button">LOGIN
+                    </button>
+                </form>
             </article>
         </section>
     </>);
