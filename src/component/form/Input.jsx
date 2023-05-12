2a4e6db705d2442669e22bd8ad9ad3d4d59f2aa6
r-barlow <rbarlow@neslotech.co.za>
2023-05-12 19:43:15 +0200

    TRAIN-266: Refactor after review.

--- src/component/form/Input.jsx
+++ src/component/form/Input.jsx
@@ -4,21 +4,18 @@ import InputRequired from "./InputRequired";
 
 import "./input.scss";
 
-const Input = ({type, placeholder, hasError, onChange}) => {
-
-    return (
-        <div className="form__input__group">
-            <input type={type}
-                   className={`form__control${hasError ? " form__input__error" : ""}`}
-                   placeholder={placeholder}
-                   aria-label={placeholder}
-                   onChange={e => {
-                       onChange(e.target.value);
-                   }}/>
-            {hasError ? <InputRequired/> : null}
-        </div>
-    );
-};
+const Input = ({type, placeholder, hasError, onChange}) => (
+    <div className="form__input__group">
+        <input type={type}
+               className={`form__control${hasError ? " form__input__error" : ""}`}
+               placeholder={placeholder}
+               aria-label={placeholder}
+               onChange={e => {
+                   onChange(e.target.value);
+               }}/>
+        {hasError ? <InputRequired/> : null}
+    </div>
+);
 
 Input.propTypes = {
     type: PropTypes.string.isRequired,
--- src/component/form/InputRequired.jsx
+++ src/component/form/InputRequired.jsx
@@ -2,17 +2,15 @@ import {POSITION} from "../../tool/constant";
 
 import Tooltip from "../ui/Tooltip";
 
-import { ReactComponent as ErrorIcon } from '../../icon/error.svg';
+import {ReactComponent as ErrorIcon} from '../../icon/error.svg';
 
 import "./input-required.scss";
 
-const InputRequired = () => {
-    return (
-        <div className="form__input__error__image__frame">
-            <ErrorIcon className="form__input__error__image" alt="input error"/>
-            <Tooltip text="Required" position={POSITION.RIGHT}/>
-        </div>
-    );
-}
+const InputRequired = () => (
+    <div className="form__input__error__image__frame">
+        <ErrorIcon className="form__input__error__image" alt="input error"/>
+        <Tooltip text="Required" position={POSITION.RIGHT}/>
+    </div>
+);
 
 export default InputRequired;
