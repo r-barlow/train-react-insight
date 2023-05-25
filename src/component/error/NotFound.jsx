5a34e191f3fc40ae990b5bdf6d99ffca0186ce0d
r-barlow <rbarlow@neslotech.co.za>
2023-05-25 13:17:01 +0200

    TRAIN-280: Rename error css class to not-found.

--- src/component/error/NotFound.jsx
+++ src/component/error/NotFound.jsx
@@ -8,11 +8,11 @@ const NotFound = () => {
     const navigate = useNavigate();
 
     return (
-        <section className="error__frame">
-            <h1 className="error__title">4<span className="error__title--amber">0</span>4</h1>
-            <h2 className="error__subtitle">Oh no!</h2>
-            <p className="error__description">Looks like the page doesn't exist!</p>
-            <button className="error__button" onClick={() => navigate(ROUTE.DASHBOARD)}>GO HOME</button>
+        <section className="not-found__frame">
+            <h1 className="not-found__title">4<span className="not-found__title--amber">0</span>4</h1>
+            <h2 className="not-found__subtitle">Oh no!</h2>
+            <p className="not-found__description">Looks like the page doesn't exist!</p>
+            <button className="not-found__button" onClick={() => navigate(ROUTE.DASHBOARD)}>GO HOME</button>
         </section>
     );
 }
